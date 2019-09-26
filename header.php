832fada24ab4d70a380a974bcd9b413d0b014abc
Cristian Myroniuk <nahuelmyroniuk1999@gmail.com>
2019-09-26 18:56:33 -0300

    Nuevo Footer

--- header.php
+++ header.php
@@ -3,16 +3,17 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/header.css">
+    <link href="https://fonts.googleapis.com/css?family=Michroma&display=swap" rel="stylesheet">
   </head>
   <body>
     <div class="contenedor">
       <header>
       <input type="checkbox" id="menu">
       <label for="menu"><img src="img/menu.svg" alt=""></label>
-      <a class="logo" href="index.php"><h1>DIGITAL SPORT</h1></a>
+      <a class="logo" href="index.php"><h1>DIGITAL SPORT></h1></a>
 
      <nav class="menu">
        <ul>
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Monoton&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Monoton&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Michroma&display=swap" rel="stylesheet">
+
 
 
   </head>
@@ -100,15 +100,18 @@
 
             </div>
           </div>
+
+            <?php
+            //incluyo el footer:
+            require_once('footer.html'); ?>
+
         </div>
 
 
 
-      </div>
 
-      <?php
-      //incluyo el footer:
-      require_once('footer.html'); ?>
+
+
 
 
 
